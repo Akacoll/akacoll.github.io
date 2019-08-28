7980ba195d7527d00ec7d59f4729a8e74446c521
Gabriel Collado <gabriel.akacoll@gmail.com>
2019-08-28 14:46:21 -0300

    Updates

---
+++ precache-manifest.1be2f7937619d16784ac9953ef72acda.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1437512cab37224037171db50d6a9da0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8352e4c116b80d42a4d0",
+    "url": "/static/css/main.e1c6756f.chunk.css"
+  },
+  {
+    "revision": "38e504c2599dd2705aad",
+    "url": "/static/js/2.ac334eea.chunk.js"
+  },
+  {
+    "revision": "8352e4c116b80d42a4d0",
+    "url": "/static/js/main.10c565a6.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  }
+]);
