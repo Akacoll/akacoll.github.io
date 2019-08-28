266d8d0be0b874f8cb38da0f1c6b5f30708b4488
Gabriel Collado <gabriel.akacoll@gmail.com>
2019-08-28 11:50:28 -0300

    Minor fixes

---
+++ build/precache-manifest.0a15b6f70cbbd9640e904bd9b75847f3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "303d3b5da33ff4b485a42745ed709000",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bbcbfcecb16d91d277a7",
+    "url": "/static/css/main.e1c6756f.chunk.css"
+  },
+  {
+    "revision": "bd33e965c2c61100e7ee",
+    "url": "/static/js/2.fae4d1c5.chunk.js"
+  },
+  {
+    "revision": "bbcbfcecb16d91d277a7",
+    "url": "/static/js/main.e9144dbb.chunk.js"
+  },
+  {
+    "revision": "85304a0d8147cfb7967e",
+    "url": "/static/js/runtime~main.077bb605.js"
+  }
+]);
